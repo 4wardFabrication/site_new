ba0628077bee630f7da49d06dfd7ec84f4b918b6
Anthony Sceresini <asceresini@gmail.com>
2015-02-23 18:51:37 +1100

    deploy to staging setup

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -1,1 +1,8 @@
-exit 1
+#!/bin/bash
+
+HEROKU_APP=$1
+heroku apps | grep "^$HEROKU_APP\b" || heroku create --no-remote "$HEROKU_APP" --stack 'cedar'
+git init .
+git add --all
+git commit -m "Site update at $(date '+%d/%m/%Y %H:%M:%S')"
+git push --force "git@heroku.com:$HEROKU_APP.git" 'master:master'
