b7d626468432d5bb20c8e5f9854826634fa7d998
Anthony Sceresini <asceresini@gmail.com>
2015-02-24 20:24:11 +1100

    can now process paths and render jade markdown

--- app/koa.js
+++ app/koa.js
@@ -1,12 +1,31 @@
-var logger = require('koa-logger'),
+var fs = require('fs'),
+    jade = require('jade'),
+    logger = require('koa-logger'),
     send = require('koa-send'),
     koa = require('koa'),
-    app = koa();
+    app = koa(),
+
+    port = process.env.PORT || 3000;
 
 app.use(logger());
 
 app.use(function *() {
-  this.body = 'Hello world';
+  var candidatePath = __dirname + '/www' + this.path;
+  try {
+    var stats = fs.statSync(candidatePath);
+    if(stats.isDirectory()) {
+      this.body = jade.renderFile(candidatePath + '/index.jade');
+    }
+
+    if(stats.isFile()) {
+      console.log(candidatePath);
+      yield send(this, candidatePath);
+    }
+  } catch(err) {
+    this.status = 404
+    this.body = 'Not Found';
+  }
 });
 
-app.listen(process.env.PORT || 3000);
+app.listen(port);
+console.log('Listening on port ' + port);
