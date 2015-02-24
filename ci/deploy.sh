27e3330507a26bdd5d672bd2abda66bcedc928ad
Anthony Sceresini <asceresini@gmail.com>
2015-02-25 08:51:06 +1100

    refactored to self document

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -1,10 +1,28 @@
 #!/bin/bash
-
-HEROKU_APP=$1
+APP=$1
 NODE_ENV=$2
-heroku apps | grep "^$HEROKU_APP\b" || heroku create --no-remote "$HEROKU_APP" --stack 'cedar'
-heroku config:set "NODE_ENV=$NODE_ENV" --app "$HEROKU_APP"
-git init .
-git add --all
-git commit -m "Site update at $(date '+%d/%m/%Y %H:%M:%S')"
-git push --force "git@heroku.com:$HEROKU_APP.git" 'master:master'
+STACK='cedar'
+
+function verify_or_create_app() {
+  local _APP=$1
+  local _STACK=$2
+  heroku apps | grep "^$_APP\b" || heroku create --no-remote "$_APP" --stack "$_STACK"
+}
+
+function set_node_env() {
+  local _APP=$1
+  local _NODE_ENV=$2
+  heroku config:set "NODE_ENV=$_NODE_ENV" --app "$_APP"
+}
+
+function deploy_to_heroku() {
+  local _APP=$1
+  git init .
+  git add --all
+  git commit -m "Site update at $(date '+%d/%m/%Y %H:%M:%S')"
+  git push --force "git@heroku.com:$_APP.git" 'master:master'
+}
+
+verify_or_create_app $APP $STACK
+set_node_env $APP $NODE_ENV
+deploy_to_heroku $APP
