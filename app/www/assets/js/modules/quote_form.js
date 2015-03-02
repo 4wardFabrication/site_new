30805288f8fd228425a1ecb8752190e21ec4b872
Anthony Sceresini <asceresini@gmail.com>
2015-03-02 21:51:10 +1100

    adding js for quoteform

--- app/www/assets/js/modules/quote_form.js
+++ app/www/assets/js/modules/quote_form.js
@@ -1,86 +1,84 @@
 function QuoteForm(element, url, alerts) {
-  this.element = element;
-  this.apiUrl = url;
-  this.fields = {
-    subject: this.element.find('#subject'),
-    from: this.element.find('#from'),
-    body: this.element.find('#body')
-  };
-  this.alerts = alerts;
-  this.messages = {
-    invalid_form: '<strong>Uh oh!</strong> Please enter an email and some details about your request.',
-    send_pending: '<strong>Hold up!</strong> We are attempting to send your request.',
-    send_success: '<strong>Got it!</strong> We will respond within 48 hours. Thank you!',
-    send_error: '<strong>Well this is embarrassing!</strong> There was an issue sending your request. We apologise for any inconvenience.'
-  };
-  this.validations = {
-    email: new RegExp('^[^@]+@[^@]+$'),
-    body: new RegExp('.{5}')
-  };
+  var _ = {
+      element: element,
+      apiUrl: url,
+      domComponents: {
+        form: element.find('form'),
+        subject: element.find('input#subject'),
+        from: element.find('input#from'),
+        body: element.find('textarea#body'),
+        send: element.find('button#send')
+      },
+      alerts: alerts,
+      messages: {
+        invalid_form: '<strong>Uh oh!</strong> Please enter an email and some details about your request.',
+        send_pending: '<strong>Hold up!</strong> We are attempting to send your request.',
+        send_success: '<strong>Got it!</strong> We will respond within 48 hours. Thank you!',
+        send_error: '<strong>Well this is embarrassing!</strong> There was an issue sending your request. We apologise for any inconvenience.'
+      },
+      validations: {
+        email: new RegExp('^[^@]+@[^@]+$'),
+        body: new RegExp('.{5}')
+      },
+      divertKeyPress: function(form, cb) {
+        form.submit(function() {
+          cb();
+          return false;
+        });
+      },
+      send: function() {
+        if(_.validate()) {
+          _.alerts.showInfo(self.messages.send_pending);
+          $.ajax({
+            type: 'POST',
+            url: _.apiUrl,
+            data: JSON.stringify({
+              subject: _.domComponents.subject.val(),
+              from: _.domComponents.from.val(),
+              body: _.domComponents.body.val()
+            })
+          })
+          .success(function() {
+            _.alerts.showSuccess(_.messages.send_success);
+          })
+          .error(function() {
+            _.alerts.showError(_.messages.send_error);
+          });
+        }
+      },
+      formatAsValid: function(field) {
+        field.parent().removeClass('has-error');
+      },
+      formatAsInvalid: function(field) {
+        field.parent().addClass('has-error');
+      },
+      validateFrom: function() {
+        return _.validateField(_.validations.email, _.domComponents.from);
+      },
+      validateBody: function() {
+        return _.validateField(_.validations.body, _.domComponents.body);
+      },
+      validateField: function(regex, field) {
+        if(regex.test(field.val())) {
+          _.formatAsValid(field);
+          return true;
+        }
+        _.formatAsInvalid(field);
+        return false;
+      },
+      validate: function() {
+        var valid = _.validateFrom() & _.validateBody();
+        if(valid) {
+          _.alerts.hide();
+        } else {
+          _.alerts.showError(_.messages.invalid_form);
+        }
+        return valid;
+      }
+    }
 
-  function divertKeyPress(self) {
-    self.element.find('form').submit(function() {
-      self.send();
-      return false;
-    });
-  };
-
-  divertKeyPress(this);
-};
-
-QuoteForm.prototype.send = function() {
-  var self = this;
-  if(self.validate()) {
-    self.alerts.showInfo(self.messages.send_pending);
-    $.ajax({
-      type: 'POST',
-      url: self.apiUrl,
-      data: JSON.stringify({
-        subject: self.fields.subject.val(),
-        from: self.fields.from.val(),
-        body: self.fields.body.val()
-      })
-    })
-    .success(function() {
-      self.alerts.showSuccess(self.messages.send_success);
-    })
-    .error(function() {
-      self.alerts.showError(self.messages.send_error);
-    });
-  }
-};
-
-QuoteForm.prototype.formatAsValid = function(field) {
-  field.parent().removeClass('has-error');
-};
-
-QuoteForm.prototype.formatAsInvalid = function(field) {
-  field.parent().addClass('has-error');
-};
-
-QuoteForm.prototype.validateFrom = function() {
-  return this.validateField(this.validations.email, this.fields.from);
-};
-
-QuoteForm.prototype.validateBody = function() {
-return this.validateField(this.validations.body, this.fields.body);
-};
-
-QuoteForm.prototype.validateField = function(regex, field) {
-  if(regex.exec(field.val())) {
-    this.formatAsValid(field);
-    return true;
-  }
-  this.formatAsInvalid(field);
-  return false;
-};
-
-QuoteForm.prototype.validate = function() {
-  var valid = this.validateFrom() & this.validateBody();
-  if(valid) {
-    this.alerts.hide();
-  } else {
-    this.alerts.showError(this.messages.invalid_form);
-  }
-  return valid;
+  _.divertKeyPress(_.domComponents.form, _.send);
+  _.domComponents.from.on('change', _.validateFrom);
+  _.domComponents.body.on('change', _.validateBody);
+  _.domComponents.send.on('click', _.send);
 };
