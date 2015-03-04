826dd9a3ebc1b70ed15f3cbf2d26497db56697c1
Anthony Sceresini <asceresini@gmail.com>
2015-03-05 08:34:08 +1100

    testing send success

--- test/emailer_test.js
+++ test/emailer_test.js
@@ -1,17 +1,24 @@
-module.exports = {
-  setUp: function(callback) {
+var Emailer = require('../app/lib/emailer.js');
+var statusCode = {
+  Ok: 200,
+  BadRequest: 400,
+  BadGateway: 502
+};
 
-    callback();
-  },
-
-  tearDown: function(callback) {
-
-    callback();
-  },
-
-  testSomething: function(test) {
-    test.expect(1);
-    test.ok(true, true);
-    test.done();
-  }
-}
+exports.testEmailerResponseIsSuccess = function(test) {
+  var mailgun = {
+        messages: function() {
+          return {send:function(data, cb){cb(undefined, data);}}
+        }
+      },
+      data = {
+        to: 'to@email.com',
+        from: 'from@email.com',
+        subject: 'a subject',
+        text: 'Some text',
+      };
+  test.expect(1);
+  Emailer(data, mailgun).send()
+    .then(function(successCode) { test.equals(successCode, statusCode.Ok, 'Must be HTTP Success: 200'); test.done();})
+    .catch(function(err) { test.equals(err, statusCode.Ok, 'Must be HTTP Success: 200'); test.done();})
+};
