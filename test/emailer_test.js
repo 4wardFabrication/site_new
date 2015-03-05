197b037e802feae131acfe8cbbdd93fc683ad2c3
Anthony Sceresini <asceresini@gmail.com>
2015-03-05 18:08:07 +1100

    test for invalid to email address

--- test/emailer_test.js
+++ test/emailer_test.js
@@ -5,25 +5,60 @@ var statusCode = {
   BadGateway: 502
 };
 
-exports.validScenarios = {
-  testEmailerResponseIsOk: function(test) {
-    var mailgun = {
-          messages: function() {
-            return {send:function(data, cb){cb(undefined, data);}}
-          }
-        },
-        data = {
-          to: 'to@email.com',
-          from: 'from@email.com',
-          subject: 'a subject',
-          text: 'Some text',
-        };
-    test.expect(1);
-    Emailer(data, mailgun).send()
-      .then(function(successCode) { test.equals(successCode, statusCode.Ok, 'Must be HTTP Success: 200'); test.done();})
-      .catch(function(err) { test.equals(err, statusCode.Ok, 'Must be HTTP Success: 200'); test.done();})
-  }
-}
+module.exports = {
+  validScenarios: {
+    testEmailerResponseIsOk: function(test) {
+      var mailgun = {
+            messages: function() {
+              return {send:function(data, cb){cb(undefined, data);}}
+            }
+          },
+          data = {
+            to: 'to@email.com',
+            from: 'from@email.com',
+            subject: 'a subject',
+            text: 'Some text',
+          };
+      test.expect(1);
+      Emailer(data, mailgun).send()
+        .then(function(successCode) {
+          test.equals(successCode, statusCode.Ok, 'Must be HTTP Success: 200');
+          test.done();
+        })
+        .catch(function(err) {
+          test.equals(err, statusCode.Ok, 'Must be HTTP Success: 200');
+          test.done();
+        });
+    }
+  },
 
-exports.inValidScenarios = {
+  invalidScenarios: {
+    setUp: function(callback) {
+      this.mailgun = {};
+      this.data = {
+        to: '@email.com',
+        from: 'from@email.com',
+        subject: 'a subject',
+        text: 'Some text',
+      };
+      callback();
+    },
+
+    tearDown: function(callback) {
+      callback();
+    },
+
+    testEmailerResponseIsBadRequestWithInvalidToAddress: function(test) {
+      test.expect(1);
+      Emailer(this.data, this.mailgun).send()
+        .then(function(successCode) {
+          test.equals(successCode, statusCode.BadRequest, 'Must be HTTP BadRequest: 400');
+          test.done();
+        })
+        .catch(function(err) {
+          test.equals(err, statusCode.BadRequest, 'Must be HTTP BadRequest: 400');
+          test.done();
+        });
+    }
+  }
 };
