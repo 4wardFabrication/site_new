00ef70194d0a420080106e06bd4b2541b6f3bcb8
Anthony Sceresini <asceresini@gmail.com>
2015-03-23 18:13:58 +1100

    initial test to check showSuccess works

--- test/jasmine/modules/alerts_test.js
+++ test/jasmine/modules/alerts_test.js
@@ -1,5 +1,20 @@
 describe('Alerts', function() {
-  it('is true', function() {
-    expect(true).toBe(true);
+  var alerts, element;
+
+  beforeEach(function() {
+    element = $('<div id="alerts"><div role="alert" class="alert alert-success"><span></span></div><div role="alert" class="alert alert-info"><span></span></div><div role="alert" class="alert alert-danger"><span></span></div></div>');
+    alerts = new Alerts(element);
   });
+
+  describe('#showSuccess', function(){
+    beforeEach(function() {
+        alerts.showSuccess('success message');
+    });
+
+    it('success message is shown', function() {
+      expect(element).toEqual($('<div id="alerts"><div role="alert" class="alert alert-success" style="display: block; "><span>success message</span></div><div role="alert" class="alert alert-info" style="display: none; "><span></span></div><div role="alert" class="alert alert-danger" style="display: none; "><span></span></div></div>'));
+    });
+
+  });
+
 });
