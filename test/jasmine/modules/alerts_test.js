b4e44a6b1492c5f7050b546eef64ef7cdafecb28
Anthony Sceresini <asceresini@gmail.com>
2015-03-23 18:16:49 +1100

    no need for nesting

--- test/jasmine/modules/alerts_test.js
+++ test/jasmine/modules/alerts_test.js
@@ -6,15 +6,8 @@ describe('Alerts', function() {
     alerts = new Alerts(element);
   });
 
-  describe('#showSuccess', function(){
-    beforeEach(function() {
-        alerts.showSuccess('success message');
-    });
-
-    it('success message is shown', function() {
-      expect(element).toEqual($('<div id="alerts"><div role="alert" class="alert alert-success" style="display: block; "><span>success message</span></div><div role="alert" class="alert alert-info" style="display: none; "><span></span></div><div role="alert" class="alert alert-danger" style="display: none; "><span></span></div></div>'));
-    });
-
+  it('success message is shown', function() {
+    alerts.showSuccess('success message');
+    expect(element).toEqual($('<div id="alerts"><div role="alert" class="alert alert-success" style="display: block; "><span>success message</span></div><div role="alert" class="alert alert-info" style="display: none; "><span></span></div><div role="alert" class="alert alert-danger" style="display: none; "><span></span></div></div>'));
   });
-
 });
