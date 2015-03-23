dc71c3bfa337b92dd1922c249d69bb0cdcf33da2
Anthony Sceresini <asceresini@gmail.com>
2015-03-23 18:20:39 +1100

    testing showError, showInfo and hide

--- test/jasmine/modules/alerts_test.js
+++ test/jasmine/modules/alerts_test.js
@@ -10,4 +10,19 @@ describe('Alerts', function() {
     alerts.showSuccess('success message');
     expect(element).toEqual($('<div id="alerts"><div role="alert" class="alert alert-success" style="display: block; "><span>success message</span></div><div role="alert" class="alert alert-info" style="display: none; "><span></span></div><div role="alert" class="alert alert-danger" style="display: none; "><span></span></div></div>'));
   });
+
+  it('Error message is shown', function() {
+    alerts.showError('error message');
+    expect(element).toEqual($('<div id="alerts"><div role="alert" class="alert alert-success" style="display: none; "><span></span></div><div role="alert" class="alert alert-info" style="display: none; "><span></span></div><div role="alert" class="alert alert-danger" style="display: block; "><span>error message</span></div></div>'));
+  });
+
+  it('Info message is shown', function() {
+    alerts.showInfo('info message');
+    expect(element).toEqual($('<div id="alerts"><div role="alert" class="alert alert-success" style="display: none; "><span></span></div><div role="alert" class="alert alert-info" style="display: block; "><span>info message</span></div><div role="alert" class="alert alert-danger" style="display: none; "><span></span></div></div>'));
+  });
+
+  it('All alerts are hidden', function() {
+    alerts.hide();
+    expect(element).toEqual($('<div id="alerts"><div role="alert" class="alert alert-success" style="display: none; "><span></span></div><div role="alert" class="alert alert-info" style="display: none; "><span></span></div><div role="alert" class="alert alert-danger" style="display: none; "><span></span></div></div>'));
+  });
 });
