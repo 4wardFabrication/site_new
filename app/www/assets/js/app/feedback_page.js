aeddbc036cf6236870d127ea89db592a28cf9994
Anthony Sceresini <asceresini@gmail.com>
2015-03-28 13:37:32 +1100

    adding leadtime, puncuality and professionalism

--- app/www/assets/js/app/feedback_page.js
+++ app/www/assets/js/app/feedback_page.js
@@ -10,6 +10,9 @@
         ratings: {
           workmanship: contactForm.find('#ratings-workmanship'),
           communication: contactForm.find('#ratings-communication'),
+          leadtime: contactForm.find('#ratings-leadtime'),
+          punctuality: contactForm.find('#ratings-punctuality'),
+          professionalism: contactForm.find('#ratings-professionalism'),
         },
         comments: contactForm.find('textarea#comments'),
         send: contactForm.find('button#send')
@@ -46,12 +49,27 @@
         return form.validateRadio(domComponents.ratings.communication);
       },
 
+      validateLeadTime = function() {
+        return form.validateRadio(domComponents.ratings.leadtime);
+      },
+
+      validatePunctuality = function() {
+        return form.validateRadio(domComponents.ratings.punctuality);
+      },
+
+      validateProfessionalism = function() {
+        return form.validateRadio(domComponents.ratings.professionalism);
+      },
+
       validate = function() {
         var valid = validateFrom() &
                     validateComments() &
                     validateName() &
                     validateWorkmanship() &
-                    validateCommunication();
+                    validateCommunication() &
+                    validateLeadTime() &
+                    validatePunctuality() &
+                    validateProfessionalism();
 
         if(valid) {
           alerts.hide();
@@ -67,6 +85,9 @@
           'Name: ' + domComponents.name.val(),
           'Workmanship: ' + domComponents.ratings.workmanship.find('input:checked').val(),
           'Communication: ' + domComponents.ratings.communication.find('input:checked').val(),
+          'Lead time: ' + domComponents.ratings.leadtime.find('input:checked').val(),
+          'Punctuality: ' + domComponents.ratings.punctuality.find('input:checked').val(),
+          'Professionalism: ' + domComponents.ratings.professionalism.find('input:checked').val(),
           'Comments: ' + domComponents.comments.val()
         ].join('\n');
       },
@@ -101,5 +122,8 @@
   domComponents.name.on('change', validateName);
   domComponents.ratings.workmanship.on('change', validateWorkmanship);
   domComponents.ratings.communication.on('change', validateCommunication);
+  domComponents.ratings.leadtime.on('change', validateLeadTime);
+  domComponents.ratings.punctuality.on('change', validatePunctuality);
+  domComponents.ratings.professionalism.on('change', validateProfessionalism);
   domComponents.send.on('click', send);
 })(this);
